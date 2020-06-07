9d80dec45ede27b99f34773b6957b54de8de8360
dearCX <997390859@qq.com>
2020-06-08 00:20:14 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div id="cover">
+      <Header></Header>
+      <Todo></Todo>
+      <Footer></Footer>
+    </div>
+  </div>
+</template>
+<script>
+import Header from './views/header.vue'
+import Footer from './views/footer.jsx'
+import Todo from './views/todo.vue'
+export default {
+  components: {
+    Header,
+    Footer,
+    Todo
+  },
+  data() {
+    return{
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+#cover{
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background-color: pink;
+  opacity: 0.45;
+}
+</style>
